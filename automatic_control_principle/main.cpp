c6fb2663a9d1436031340d9e3f472ee9fcd77376
PkFrTk <109774802+PkFrTk@users.noreply.github.com>
2023-02-21 23:01:54 +0800

    修改bug

--- automatic_control_principle/main.cpp
+++ automatic_control_principle/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #define PI 3.14
+using namespace std;
 class Dynamic_Indicators
 {
 public:
@@ -9,28 +10,28 @@ public:
 	// Constructor
 	Dynamic_Indicators()
 	{
-		std::cout << "Constructor is call" << std::endl;
+		cout << "Constructor is call" << endl;
 	}
 	~Dynamic_Indicators()
 	{
-		std::cout << "Destructor is call" << std::endl;
+		cout << "Destructor is call" << endl;
 	}
 	// Menber Function
 	void setParameters1()
 	{
-		std::cout << "请输入频率ωn，超调量σ:" << std::endl;
-		std::cout << "频率ωn:";
-		std::cin >> wn;
-		std::cout << "超调量σ(100%):";
-		std::cin >> overshoot;
+		cout << "请输入频率ωn，超调量σ:" << endl;
+		cout << "频率ωn:";
+		cin >> wn;
+		cout << "超调量σ(100%):";
+		cin >> overshoot;
 	}
 	void selectCalculateMode()
 	{
 		int select;
-		std::cout << "请选择计算模式：" << std::endl;
-		std::cout << "1.已知二阶系统的超调量σ，频率ωn" << std::endl;
-		std::cout << "2.已知二阶系统的频率wn/ψ" << std::endl;
-		std::cin >> select;
+		cout << "请选择计算模式：" << endl;
+		cout << "1.已知二阶系统的超调量σ，频率ωn" << endl;
+		cout << "2.已知二阶系统的频率wn/ψ" << endl;
+		cin >> select;
 		if (select == 1)
 		{ // second order system's dynamic indicators known
 			setParameters1();
@@ -48,9 +49,9 @@ public:
 		zeta = sqrt(zeta_square);
 		rise_time = (PI - phi) / (wn * sqrt(1 - pow(zeta, 2)));
 		peak_time = PI / (wn * sqrt(1 - pow(zeta, 2)));
-		std::cout << "调节时间误差范围为2%？5%？" << std::endl;
-		std::cout << "Δ=";
-		std::cin >> deta;
+		cout << "调节时间误差范围为2%？5%？" << endl;
+		cout << "Δ=";
+		cin >> deta;
 		if (deta == 5)
 		{
 			adjusting_time = 3.5 / (zeta * wn);
@@ -64,20 +65,23 @@ public:
 	void printResult();
 	double calculateAllInOne(double wn, double phi, double zeta, double deta);
 	double incalculateAllInOne(double overshoot, double peak_time);
+
 private:
 	double wn, phi, zeta, deta;
 	double rise_time, peak_time, overshoot, adjusting_time, number_of_oscillations;
 };
-void Dynamic_Indicators::printResult(){
-	std::cout << "-----------------------" << std::endl;
-	std::cout << "上升时间tr：" << rise_time << "s" << std::endl;
-	std::cout << "峰值时间tp：" << peak_time << "s" << std::endl;
-	std::cout << "超调量σ(100%)：" << overshoot*100 << "%" << std::endl;
-	std::cout << "调节时间ts：" << adjusting_time << "s" << "（误差Δ=" << deta << "%）" << std::endl;
-	std::cout << "振荡次数N：" << number_of_oscillations << std::endl;
-	std::cout << "-----------------------" << std::endl;
+void Dynamic_Indicators::printResult()
+{
+	cout << "-----------------------" << endl;
+	cout << "上升时间tr：" << rise_time << "s" << endl;
+	cout << "峰值时间tp：" << peak_time << "s" << endl;
+	cout << "超调量σ(100%)：" << overshoot * 100.00 << endl;
+	cout << "调节时间ts：" << adjusting_time << "s" << endl;
+	cout << "振荡次数N：" << number_of_oscillations << endl;
+	cout << "-----------------------" << endl;
 }
-double Dynamic_Indicators::calculateAllInOne(double wn, double phi, double zeta, double deta){
+double Dynamic_Indicators::calculateAllInOne(double wn, double phi, double zeta, double deta)
+{
 	rise_time = (PI - phi) / (wn * sqrt(1 - pow(zeta, 2)));
 	peak_time = PI / (wn * sqrt(1 - pow(zeta, 2)));
 	overshoot = exp((-PI * zeta) / (sqrt(1 - pow(zeta, 2))));
